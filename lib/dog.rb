f9d2f92d0f5568aa56533feb932691b974877863
Justin Pierce <jwpierce1984@gmail.com>
2020-01-27 17:53:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,7 @@
 class Dog
-  attr_accessor :name
   
   @@all = []
   
-  def initialize(name)
-    @name = name
-    save
-  end
-  def self.all
-    @@all
-  end
-  def self.print_all
-    puts @@all.map { |dog| dog.name }
-  end
-  def self.clear_all
-    @@all.clear
-  end
-  def save
-    @@all << self
-  end
+  attr_accessor :name
+  
 end
